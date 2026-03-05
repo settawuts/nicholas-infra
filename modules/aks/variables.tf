7d6d39066759e02f0a9a14d9e27299dfa903bb6e
Settawuo <settawuo@ais.co.th>
2026-03-05 13:08:25 +0700

    fix

--- modules/aks/variables.tf
+++ modules/aks/variables.tf
@@ -9,19 +9,29 @@ variable "application_gateway_id"          { type = string }
 variable "log_analytics_workspace_id"      { type = string }
 variable "admin_group_object_ids"          { type = list(string) }
 variable "api_server_authorized_ip_ranges" { type = list(string) }
-variable "acr_geo_replication_location"    { type = string; default = "southeastasia" }
-variable "service_cidr"                    { type = string; default = "10.100.0.0/16" }
-variable "dns_service_ip"                  { type = string; default = "10.100.0.10" }
+variable "acr_geo_replication_location"    { type = string
+                                             default = "southeastasia" }
+variable "service_cidr"                    { type = string
+                                             default = "10.100.0.0/16" }
+variable "dns_service_ip"                  { type = string
+                                             default = "10.100.0.10" }
 
-variable "system_node_vm_size"    { type = string; default = "Standard_DS3_v2" }
-variable "system_node_min_count"  { type = number; default = 3 }
-variable "system_node_max_count"  { type = number; default = 5 }
+variable "system_node_vm_size"    { type = string
+                                     default = "Standard_DS3_v2" }
+variable "system_node_min_count"  { type = number
+                                     default = 3 }
+variable "system_node_max_count"  { type = number
+                                     default = 5 }
 
-variable "app_node_vm_size"       { type = string; default = "Standard_D4s_v3" }
-variable "app_node_min_count"     { type = number; default = 3 }
-variable "app_node_max_count"     { type = number; default = 20 }
-
-variable "spot_node_vm_size"      { type = string; default = "Standard_D4s_v3" }
-variable "spot_node_max_count"    { type = number; default = 10 }
+variable "app_node_vm_size"       { type = string
+                                     default = "Standard_D4s_v3" }
+variable "app_node_min_count"     { type = number
+                                     default = 3 }
+variable "app_node_max_count"     { type = number
+                                     default = 20 }
+variable "spot_node_vm_size"      { type = string
+                                     default = "Standard_D4s_v3" }
+variable "spot_node_max_count"    { type = number
+                                     default = 10 }
 
 variable "tags"                   { type = map(string) }
